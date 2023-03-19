72c9d3400aac7b613b3e14067a0f3e8e5d0a2aa2
Ipshita S <ipshitashetty@gmail.com>
2023-03-19 19:14:02 +0530

    CH3 adding navbar

--- index.js
+++ index.js
@@ -1,56 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const reactHeading = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [
-    React.createElement(
-      "h1",
-      {
-        id: "title",
-        key: "h1",
-      },
-      "heading 1 from Parcel"
-    ),
-    React.createElement(
-      "h2",
-      {
-        id: "title",
-        key: "h2",
-      },
-      "heading 2"
-    ),
-    React.createElement(
-      "h3",
-      {
-        id: "title",
-        key: "h3",
-      },
-      "heading 3"
-    ),
-  ]
-);
-
-const jsxHeading = (
-  <div>
-    <h1>heading 1 from JSX</h1>
-    <h2>heading 2 from JSX</h2>
-    <h3>heading 3 from JSX</h3>
-  </div>
-);
+//import logo from "Image/logo.png";
+import "./index.css";
+import { BsFillBookFill } from "react-icons/Bs";
 
 const Title = () => {
   return <h1>Namaste React</h1>;
 };
+
+const NavBar = () => {
+  return (
+    <div className="topnav">
+      <img className="logo" />
+      <input className="searchBar" type="text" placeholder="Search.."></input>
+      <BsFillBookFill className="userIcon" />
+    </div>
+  );
+};
 const ReactComponent = () => {
   return (
     <div>
-      {Title()}
+      <NavBar />
       <Title />
-      {<Title />}
       <h1>heading 1 from Component</h1>
       <h2>heading 2 from Component</h2>
       <h3>heading 3 from Component</h3>
@@ -59,6 +30,5 @@ const ReactComponent = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(reactHeading);
-root.render(jsxHeading);
+
 root.render(<ReactComponent />);
